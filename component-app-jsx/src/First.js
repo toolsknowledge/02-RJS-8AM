efd051cd679c9394ca9e178fe479dbc18253344e
Sambasivarao <samba.pendela@gmail.com>
2021-09-30 08:48:30 +0530

    code

---
+++ component-app-jsx/src/First.js
@@ -0,0 +1,11 @@
+import React from "react";
+function First(){
+    return(
+        <React.Fragment>
+            <p>welcome to reactjs</p>
+            <span>welcome to jsx approach</span>
+            <div>welcome to functional level components</div>
+        </React.Fragment>
+    )
+};
+export default First;
