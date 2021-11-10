f13b39a28df54bcb77c921ecdc8b3b1f0a623311
Sambasivarao <samba.pendela@gmail.com>
2021-11-10 08:33:58 +0530

    code

---
+++ charts-app/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {Bar,Line,Pie} from "react-chartjs-2";
+function App() {
+  return (
+    <div>
+        <Pie width={400}
+             height={600}
+             options={{
+               maintainAspectRatio:false
+             }}
+             data={{
+               labels:["Angular","ReactJS","VueJS","JavaScript","NodeJS","MongoDB"],
+               datasets:[{
+                  label:"AshokIT",
+                  data:[12,6,3,10,11,8,4],
+                  backgroundColor:"red",
+                  borderColor:"green",
+                  borderWidth:2
+               },
+               {
+                  label: 'Quantity',
+                  data: [47, 52, 67, 58, 9, 50],
+                  backgroundColor: 'orange',
+                  borderColor: 'red'
+               },
+               {
+                label: 'Price',
+                data: [20, 52, 67, 58, 9, 25],
+                backgroundColor: 'pink',
+                borderColor: 'red'
+             }
+              ]
+             }}>
+          
+        </Pie>        
+    </div>
+  );
+}
+
+export default App;
